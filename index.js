25465cbe9115e4b7747af2efc064864ebdb67d55
selenium39 <a896585122@gmail.com>
2022-01-06 23:07:49 +0800

    stack

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const AraryStack = require('./lib/stack/array_stack')
 const ArrayStack = require('./lib/stack/array_stack')
 const LinkedStack = require('./lib/stack/linked_stack')
 module.exports = {
   ArrayStack,
   LinkedStack,
-  Stack: AraryStack
+  Stack: ArrayStack
 }
